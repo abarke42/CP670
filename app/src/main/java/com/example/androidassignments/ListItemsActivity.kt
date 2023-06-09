aec867104be591ab6f0edc27352fbc1030e5974b
Alyssa <wilk4149@mylaurier.ca>
2023-06-08 23:36:23 -0400

    Added functions for activities and debugging with Logcat to monitor the activity flow.

--- app/src/main/java/com/example/androidassignments/ListItemsActivity.kt
+++ app/src/main/java/com/example/androidassignments/ListItemsActivity.kt
@@ -2,10 +2,38 @@ package com.example.androidassignments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class ListItemsActivity : AppCompatActivity() {
+    private val tag = "ListItemsActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_items)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(tag, "Started ListItemsActivity.")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(tag, "Paused ListItemsActivity.")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(tag, "Resumed ListItemsActivity.")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(tag, "Stopped ListItemsActivity.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(tag, "Destroyed ListItemsActivity.")
     }
 }
--- app/src/main/java/com/example/androidassignments/MainActivity.kt
+++ app/src/main/java/com/example/androidassignments/MainActivity.kt
@@ -1,13 +1,41 @@
 package com.example.androidassignments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 
 //Main activity to launch the app
 class MainActivity : ComponentActivity() {
+
+    private val tag = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //Calls activity_main.xml
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(tag, "Started MainActivity.")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(tag, "Paused MainActivity.")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(tag, "Resumed MainActivity.")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(tag, "Stopped MainActivity.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(tag, "Destroyed MainActivity.")
     }
 }
