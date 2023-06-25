ea380a363f7c4cfd66b6110f7f4b22d5589f9340
Alyssa <wilk4149@mylaurier.ca>
2023-06-24 22:45:02 -0400

    Switch is working with onCheckedChange().

--- app/src/main/java/com/example/androidassignments/ListItemsActivity.java
+++ app/src/main/java/com/example/androidassignments/ListItemsActivity.java
@@ -2,17 +2,28 @@ package com.example.androidassignments;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.androidassignments.R.layout;
 
 import org.jetbrains.annotations.Nullable;
 
-public final class ListItemsActivity extends AppCompatActivity {
+public final class ListItemsActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
     private final String tag = "ListItemsActivity";
+    Switch list_switch;
+    private boolean isChecked;
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(layout.activity_list_items);
+
+        //Switch business
+        list_switch=findViewById(R.id.list_items_switch);
+        list_switch.setOnCheckedChangeListener(this);
     }
 
     protected void onStart() {
@@ -38,5 +49,27 @@ public final class ListItemsActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.i(this.tag, "Destroyed ListItemsActivity.");
+    }
+
+    //Required to use onCheckedChanges instead of setOnCheckedChanges to implement the CompoundButton class
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+        if(isChecked)    //Switch is on
+        {
+            CharSequence text = "Switch is On";// "Switch is Off"
+            int duration = Toast.LENGTH_SHORT; //= Toast.LENGTH_LONG if Off
+
+            Toast toast = Toast.makeText(this, text, duration); //this is the ListActivity
+            toast.show(); //display your message box
+        }
+        else    //Switch is off
+        {
+            CharSequence text = "Switch is Off";
+            int duration = Toast.LENGTH_LONG;
+
+            Toast toast = Toast.makeText(this, text, duration);
+            toast.show();
+        }
     }
 }
