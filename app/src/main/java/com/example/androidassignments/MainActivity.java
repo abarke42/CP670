a05182f3e9f8c5c831f2668394aacc828b218b19
Alyssa <wilk4149@mylaurier.ca>
2023-06-24 21:22:01 -0400

    Added onClick listener for main_button in MainActivity.

--- app/src/main/java/com/example/androidassignments/MainActivity.java
+++ app/src/main/java/com/example/androidassignments/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.androidassignments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.activity.ComponentActivity;
 import com.example.androidassignments.R.layout;
 
@@ -12,8 +16,28 @@ public final class MainActivity extends ComponentActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(layout.activity_main);
+
+        Button main_button = findViewById(R.id.main_button);
+
+        main_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                //Switch to another activity
+                Intent intent = new Intent(MainActivity.this, ListItemsActivity.class);
+                startActivityForResult(intent, 10);
+            }
+        });
+        //starts the ListItemsActivity activity using startActivityForResult( ). Use 10 for the request code.
     }
 
+    protected void onActivityResult(int requestCode, int responseCode, Intent data){
+        super.onActivityResult(requestCode, responseCode, data);
+        if(requestCode == 10)
+        {
+            Log.i("ACTIVITY_NAME", "Returned to MainActivity.onActivityResult");
+        }
+    }
     protected void onStart() {
         super.onStart();
         Log.i(this.tag, "Started MainActivity.");
