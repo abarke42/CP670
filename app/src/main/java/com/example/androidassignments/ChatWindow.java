ab2fa5f3305884bcab87508fe15cbc7315d4b30f
Alyssa <wilk4149@mylaurier.ca>
2023-06-25 23:24:03 -0400

    Added onClick event for send button in ChatWindow, but messages don't send successfully.

--- app/src/main/java/com/example/androidassignments/ChatWindow.java
+++ app/src/main/java/com/example/androidassignments/ChatWindow.java
@@ -2,6 +2,7 @@ package com.example.androidassignments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -20,6 +21,7 @@ public class ChatWindow extends AppCompatActivity {
     EditText chatText;
     Button sendButton;
     ArrayList<String> chatMessageArray = new ArrayList<>();
+    ChatAdapter messageAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +31,27 @@ public class ChatWindow extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         chatText = findViewById(R.id.chatEditText);
         sendButton = findViewById(R.id.sendButton);
+        messageAdapter = new ChatAdapter(this);
 
         sendButton.setOnClickListener(view ->
         {
             String chatMessage = chatText.getText().toString();
+
             chatMessageArray.add(chatMessage);
+
+            chatMessageArray.add(chatMessage);
+            messageAdapter.notifyDataSetChanged(); //this restarts the process of getCount()/
+            chatText.setText("");
         });
     }
     private class ChatAdapter extends ArrayAdapter {
-        private static final int resource = 0;
-        ChatAdapter ca = new ChatAdapter(this.getContext());
 //    ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(this, R.layout.activity_chat_window);
+        public ChatAdapter(Context ctx) {
+            super(ctx, 0);
+        }
+
+        //private static final int resource = 0;
+        //messageAdapter = new ChatAdapter(this.getContext());
 
         public int getCount(){
             return chatMessageArray.size();
@@ -47,25 +59,22 @@ public class ChatWindow extends AppCompatActivity {
         public String getItem(int position){
             return chatMessageArray.get(position);
         }
+        @SuppressLint("InflateParams")
         public View getView(int position, View convertView, ViewGroup parent){
             LayoutInflater inflater = ChatWindow.this.getLayoutInflater();
 
             View result;
 
             if(position%2 == 0) {
-                     result = inflater.inflate(R.layout.chat_row_incoming, null);
+                result = inflater.inflate(R.layout.chat_row_incoming, null);
             }
             else
             {
-                     result = inflater.inflate(R.layout.chat_row_outgoing, null);
+                result = inflater.inflate(R.layout.chat_row_outgoing, null);
             }
             TextView message = result.findViewById(R.id.messageText);
             message.setText(   getItem(position)  ); // get the string at position
             return result;
-        }
-
-        public ChatAdapter(Context ctx) {
-            super(ctx, 0);
         }
     }
 }
