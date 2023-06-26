a4a6b26c42ed95524520603f5c95db330a20612b
Alyssa <wilk4149@mylaurier.ca>
2023-06-26 11:52:42 -0400

    ChatWindow is now working!

--- app/src/main/java/com/example/androidassignments/ChatWindow.java
+++ app/src/main/java/com/example/androidassignments/ChatWindow.java
@@ -2,9 +2,11 @@ package com.example.androidassignments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,8 +22,7 @@ public class ChatWindow extends AppCompatActivity {
     ListView listView;
     EditText chatText;
     Button sendButton;
-    ArrayList<String> chatMessageArray = new ArrayList<>();
-    ChatAdapter messageAdapter;
+    ArrayList<String> chatMessageArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,50 +32,47 @@ public class ChatWindow extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         chatText = findViewById(R.id.chatEditText);
         sendButton = findViewById(R.id.sendButton);
-        messageAdapter = new ChatAdapter(this);
 
-        sendButton.setOnClickListener(view ->
-        {
-            String chatMessage = chatText.getText().toString();
-
-            chatMessageArray.add(chatMessage);
-
-            chatMessageArray.add(chatMessage);
-            messageAdapter.notifyDataSetChanged(); //this restarts the process of getCount()/
-            chatText.setText("");
+        chatMessageArray = new ArrayList<>();
+        ChatAdapter messageAdapter = new ChatAdapter(this);
+        listView.setAdapter(messageAdapter);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String chatMessage = chatText.getText().toString().trim();
+                chatMessageArray.add(chatMessage);
+                messageAdapter.notifyDataSetChanged();
+                chatText.setText("");
+            }
         });
     }
-    private class ChatAdapter extends ArrayAdapter {
-//    ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(this, R.layout.activity_chat_window);
-        public ChatAdapter(Context ctx) {
-            super(ctx, 0);
-        }
-
-        //private static final int resource = 0;
-        //messageAdapter = new ChatAdapter(this.getContext());
-
-        public int getCount(){
-            return chatMessageArray.size();
-        }
-        public String getItem(int position){
-            return chatMessageArray.get(position);
-        }
-        @SuppressLint("InflateParams")
-        public View getView(int position, View convertView, ViewGroup parent){
-            LayoutInflater inflater = ChatWindow.this.getLayoutInflater();
-
-            View result;
-
-            if(position%2 == 0) {
-                result = inflater.inflate(R.layout.chat_row_incoming, null);
+        class ChatAdapter extends ArrayAdapter {
+            public ChatAdapter(Context ctx) {
+                super(ctx, 0);
             }
-            else
-            {
-                result = inflater.inflate(R.layout.chat_row_outgoing, null);
+
+            public int getCount() {
+                return chatMessageArray.size();
             }
-            TextView message = result.findViewById(R.id.messageText);
-            message.setText(   getItem(position)  ); // get the string at position
-            return result;
+
+            public String getItem(int position) {
+                return chatMessageArray.get(position);
+            }
+
+            @SuppressLint("InflateParams")
+            public View getView(int position, View convertView, ViewGroup parent) {
+                LayoutInflater inflater = ChatWindow.this.getLayoutInflater();
+
+                View result;
+
+                if (position % 2 == 0) {
+                    result = inflater.inflate(R.layout.chat_row_incoming, null);
+                } else {
+                    result = inflater.inflate(R.layout.chat_row_outgoing, null);
+                }
+                TextView message = result.findViewById(R.id.messageText);
+                message.setText(getItem(position)); // get the string at position
+                return result;
+            }
         }
     }
-}
---
+++ app/src/test/java/com/example/androidassignments/MainActivityTest.java
@@ -0,0 +1,40 @@
+package com.example.androidassignments;
+
+import android.app.Activity;
+
+import androidx.lifecycle.Lifecycle;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class MainActivityTest extends TestCase {
+
+   // @Test fun testEvent() {
+   //     launchActivity<MainActivity>().use {
+   //     }
+  //  }
+
+//    public void testOnCreate() {
+//        Activity = getActivity("MainActivity");
+//        activity.getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED);
+//    }
+
+    public void testOnActivityResult() {
+    }
+
+    public void testOnStart() {
+    }
+
+    public void testOnPause() {
+    }
+
+    public void testOnResume() {
+    }
+
+    public void testOnStop() {
+    }
+
+    public void testOnDestroy() {
+    }
+}
